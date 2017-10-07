314ef6ef8d5f39abbbeb29277f61b102623c3011
MechaEdgar <ee1206@hotmail.com>
2017-10-07 11:48:25 -0500

    Objects and gameController Cs
    The cube , plane and game controller object and C# script created.

---
+++ CubeController/Assets/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Initiate");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
