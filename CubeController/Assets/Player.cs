8eb5a9427f1867e67543711779d66dc5bbdd54d7
MechaEdgar <ee1206@hotmail.com>
2017-10-07 13:17:04 -0500

    Left and right movement added.
    Left and right movement added in the Player C#

---
+++ CubeController/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 3.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	//right movement
+	void Update () {
+		if (Input.GetKey("right")){
+			transform.position += Vector3.right* speed * Time.deltaTime;
+		}
+
+	//Left movement
+		if(Input.GetKey("left")){
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}		
+	}
+
