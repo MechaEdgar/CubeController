54a3048ce0bf7269fcd2112e98964c727061636e
MechaEdgar <ee1206@hotmail.com>
2017-10-09 21:05:26 -0500

    Added 4 cameras
    Sooo, i added 4 cameras and it change if you click left mouse or right mouse buttons, the camera4 follows the player.

---
+++ CubeController/Assets/ChaseCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseCamera : MonoBehaviour {
+	//Script to make the camera3 follow the player
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target);
+	}
+}
